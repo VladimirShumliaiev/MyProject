c9406ffb434d18324891b49286961a36f6778f78
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2022-08-30 13:37:19 +0300

    +30.08.2022 - add homeSlice

--- my-app/src/components/header/HeaderComponents/Home/HomeComponents/HomePostItem.jsx
+++ my-app/src/components/header/HeaderComponents/Home/HomeComponents/HomePostItem.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const HomePostItem = (props) => {
+const HomePostItem = ({id,text}) => {
     return (
-        <div key={props.id}>
+        <div key={id}>
             <img src="https://img.tsn.ua/cached/728/tsn-84ee4f9844c21d3af2b3cfbad2c9c711/thumbs/428x268/11/75/47324e04a2485d5bf16d6a549de57511.jpeg" alt=""/>
-            <span> {props.text}</span>
+            <span> {text}</span>
         </div>
     );
 };
