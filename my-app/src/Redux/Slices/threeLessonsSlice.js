040d645c9f357837e81e7bb5da36dd2ac8222084
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2022-10-10 14:05:39 +0300

    10.10.2022  threeLessons asyncThunk

--- my-app/src/Redux/Slices/threeLessonsSlice.js
+++ my-app/src/Redux/Slices/threeLessonsSlice.js
@@ -1,10 +1,36 @@
-import {createSlice} from "@reduxjs/toolkit";
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
 
+export const fetchTodo = createAsyncThunk(
+    'Todos/fetchTodo',
+    async function(_,{rejectWithValue}){
+        try {
+            const response = await fetch('https://jsonplaceholder.typicode.com/todos')
+
+            if(!response.ok){
+                throw new Error('ERROR 1')
+            }
+
+            const data = response.json()
+            return data
+
+        } catch (error) {
+            return rejectWithValue(error.message)
+        }
+    }
+
+)
+
+const setError = (state, action) => {
+    state.status = 'rejected'
+    state.error = action.payload
+}
 
 const threeLessonsSlice = createSlice({
     name: 'Todos',
     initialState: {
         list: [],
+        status: null,
+        error: null,
     },
     reducers: {
         addTodo(state, action){
@@ -21,6 +47,17 @@ const threeLessonsSlice = createSlice({
             const toggle = state.list.find(e => e.id === action.payload.id)
             toggle.completed = !toggle.completed
         },
+    },
+    extraReducers: {
+        [fetchTodo.pending]: (state) => {
+            state.status = 'pending'
+            state.error = null
+        },
+        [fetchTodo.fulfilled]: (state, action) => {
+            state.status = 'resolved'
+            state.list = action.payload
+        },
+        [fetchTodo.rejected]: setError,
     }
 })
 
--- my-app/src/components/Main/React Lessons/React Lessons Components/Lessons/Three Lessons/Three Lessons.jsx
+++ my-app/src/components/Main/React Lessons/React Lessons Components/Lessons/Three Lessons/Three Lessons.jsx
@@ -1,20 +1,27 @@
-import React, {useState} from 'react';
-import {useDispatch} from "react-redux";
-import {addTodo} from "../../../../../../Redux/Slices/threeLessonsSlice";
+import React, {useEffect, useState} from 'react';
+import {useDispatch, useSelector} from "react-redux";
+import {addTodo,fetchTodo} from "../../../../../../Redux/Slices/threeLessonsSlice";
 import ThreeLessonsInput from "./threeLessonsComponents/ThreeLessonsInput";
 import ThreeLessonsList from "./threeLessonsComponents/ThreeLessonsList";
 import style from './ThreeLessons.module.css'
 
 const ThreeLessons = () => {
     const [title, setTitle] = useState('')
+    const {status, error} = useSelector(state => state.threeLessons)
     const dispatch = useDispatch()
     const addTask = () => {
         dispatch(addTodo({title}))
         setTitle('')
     }
+
+    useEffect(() => {
+        dispatch(fetchTodo())
+    },[dispatch])
     return (
         <div className={style.item}>
             <ThreeLessonsInput title={title} setTitle={setTitle} addTodo={addTask}/>
+            {status === 'pending' && <h2>Pending...</h2>}
+            {error && <h2>Error:{error}</h2>}
             <ThreeLessonsList/>
         </div>
     );
