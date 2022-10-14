a55b2627ca4bdeedca4a12230c6baae5d7605247
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2022-10-14 11:34:31 +0300

    14.10.2022  threeLessons asyncThunk

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import './App.css';
 import Header from "./components/header/Header";
-import Home from "./components/header/HeaderComponents/Home/Home";
 import NavBar from "./components/navBar/NavBar";
 import Footer from "./components/footer/Footer"
 import RoutingCell from "./components/RoutingCell";
@@ -13,7 +12,6 @@ function App() {
         <div className="App">
             <Header/>
             <NavBar/>
-            <Home/>
             <Footer/>
             <RoutingCell/>
         </div>
