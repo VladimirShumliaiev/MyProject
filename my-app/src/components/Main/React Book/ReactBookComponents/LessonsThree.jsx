4cc79f9be92331ed9779323a57239e16fa6b3487
VladimirShumliaiev <85706603+VladimirShumliaiev@users.noreply.github.com>
2023-02-09 13:30:02 +0200

    09.02.2023 +++

--- my-app/src/components/Main/React Book/ReactBookComponents/LessonsThree.jsx
+++ my-app/src/components/Main/React Book/ReactBookComponents/LessonsThree.jsx
@@ -1,15 +1,39 @@
 import React from 'react';
+import LessonsThreeItem from "./LessonsTrhee/LessonsThreeItem";
 
 const LessonsThree = () => {
+   
+    const arr2 = [
+        {
+            text: 'aaaaaaa',
+            b: 'b',
+            id: 1
+        },
+        {
+            text: 'a2aaaaaaa',
+            b: 'b2',
+            id: 2
+        },
+        {
+            text: 'a3aaaaaaa',
+            b: 'b3',
+            id: 3
+        },
+    ]
+
     const sandwich = {
         bread: "dutch crunch",
         meat: "tuna",
-        toppings: ['lettuce','tomato','mustard']
+        toppings: ['lettuce','tomato','mustard','4']
     }
-    const {bread, meat, toppings} = sandwich
+    const {bread, meat} = sandwich
     return (
         <div>
-            {bread} {meat} {toppings[2]}
+            {bread} {meat}
+            {
+                arr2.map((e) => <LessonsThreeItem key={e.id} {...e}/>)
+            }
+            <LessonsThreeItem/>
         </div>
     );
 };
---
+++ my-app/src/components/Main/React Book/ReactBookComponents/LessonsTrhee/LessonsThreeItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const LessonsThreeItem = ({text,b}) => {
+    return (
+        <div>
+            <div>
+                {text}
+            </div>
+            <div>
+                {b}
+            </div>
+        </div>
+    );
+};
+
+export default LessonsThreeItem;
